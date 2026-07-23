0818d74fdc83bfd54bc69c8db5a6052c4bbf0cda
jackrichter <jackrichter1@gmail.com>
2026-07-23 05:10:28 +0200

    Register our custom Reactive- AuthenticationManager with SecurityWebFilterChain, as the one to perform user credentials authentication.

--- src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/reactive/ws/users/infrastructure/WebSecurity.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.reactive.ws.users.infrastructure;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +15,8 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class WebSecurity {
 
     @Bean
-    public SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http,
+                                                          ReactiveAuthenticationManager authenticationManager) {
 
         return http
                 .authorizeExchange(exchanges -> exchanges
@@ -23,6 +25,8 @@ public class WebSecurity {
                         .anyExchange()
                         .authenticated())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)   // Disable Basic Authentication
+                .authenticationManager(authenticationManager)           // Register which AuthenticationManager to use to check user credentials.
                 .build();
     }
 
@@ -30,4 +34,20 @@ public class WebSecurity {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /**
+     * The Flow of this WebFlux Security implementation:
+     * 1. The SecurityWebFilterChain registers the AuthenticationManager (Bean ReactiveAuthenticationManager).
+     *    We create this Manager using the UserDetailsRepositoryReactiveAuthenticationManager class, as implementation,
+     *    attach to it the PasswordEncoder used to encode user password, and we want Spring Security to use it.
+     * 2. This AuthenticationManager uses a UserDetailsService through interface ReactiveUserDetailsService, which we
+     *    extend the existing UserService with.
+     * 3. UserServiceImpl then implements the method 'findByUserName' of the ReactiveUserDetailsService interface,
+     *    which is then called by the AuthenticationManager automatically.
+     * 4. We create an AuthenticationService interface to implement a method 'authenticate' that will perform the actual authentication.
+     *    In it, we use the AuthenticationManager to compare the provided password with the stored encrypted password.
+     * 5. The login endpoint in the AuthenticationController calls this 'authenticate' method to start the authentication process.
+     * OBS!
+     *    We use the email as a username in this example.
+     */
 }
